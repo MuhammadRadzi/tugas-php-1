218ade4d71cbebcf82738153eb0f59920424076a
MuhammadRadzi <nippon.pein14@gmail.com>
2026-08-20 07:51:42 +0800

    Add array and looping examples in PHP

---
+++ misc/array.php
@@ -0,0 +1,10 @@
+<?php
+
+$arrayBooks = ["Laskar Pelangi","Bumi Manusia","Negeri 5 Menara", "Durusul Lughoh", "Sang Pemimpin", "Tutor MBG"];
+$books = count($arrayBooks);
+// echo $arrayBooks[0];
+
+
+for ($i = 0; $i < $books; $i++) {
+    echo $arrayBooks [$i]. "\n";
+}
---
+++ misc/looping.php
@@ -0,0 +1,29 @@
+<?php
+
+// for
+for ($i = 1; $i <= 10; $i++) {
+    echo "Baris ke-$i\n";
+} 
+
+for ($z = 10; $z >= 1; $z--) {
+    echo "Looping ke-$z\n";
+} 
+
+//while
+$stok = 5;
+while ($stok > 0) {
+    echo "Buku dipinjam sisa stok = $stok";
+    $stok++;
+}
+
+
+
+
+
+
+
+
+
+
+
+?>
